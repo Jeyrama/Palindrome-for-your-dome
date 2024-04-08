4c242425b77ad17c762fabafab133a9ddc3aa91d
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-08 12:39:46 -0700

    Create Palindrome-for-your-dome.js

---
+++ Palindrome-for-your-dome.js
@@ -0,0 +1,15 @@
+/*
+A palindrome is a word, phrase, number, or other sequence of symbols or elements, 
+whose meaning may be interpreted the same way in either forward or reverse direction.
+Our goal is to determine whether or not a given string is a valid palindrome or not.
+
+Like the above examples, here are a few test cases which are also populated:
+  "Amore, Roma" => valid
+  "A man, a plan, a canal: Panama" => valid
+  "No 'x' in 'Nixon'" => valid
+  "Abba Zabba, you're my only friend" => invalid
+
+Notes:
+  You can see that the input is case insensitive and disregards non alphanumeric characters.
+  The empty string "" can be read forward or backward the same, it's a palindrome in our case.
+*/
