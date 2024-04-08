5be14512d4c9be2a5571668769ef210a19e1f1c9
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-08 12:41:34 -0700

    Update Palindrome-for-your-dome.js

--- Palindrome-for-your-dome.js
+++ Palindrome-for-your-dome.js
@@ -16,3 +16,9 @@ Notes:
 
 
 // Solution 
+
+function palindrome(string) {
+  let s = string.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  for (let i = 0; i < s.length/2; i++) if (s[i] != s[s.length-i-1]) return false;
+  return true;
+}
